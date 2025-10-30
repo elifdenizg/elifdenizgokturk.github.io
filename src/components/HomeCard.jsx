2cd56b0b2b6bf795c25fb3ba49c4369c78407ba8
elifdenizg <edgokturk@protonmail.com>
2025-10-30 22:15:07 +0100

    [FIX] card text colors

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,6 +1,20 @@
 //Les petits carrés de home page
 
-function HomeCard(label){
-
+function HomeCard({title,url}){
+    return(
+        <div className="text-3xl font-bold p-20 border-2 border-white w-fit h-full">
+            <a 
+                href={url} 
+                className="
+                text-white
+                visited: text-white
+                hover:text-yellow-400
+            "
+            >
+                {title}
+            </a>
+        </div>
+    );
 }
 
+export default HomeCard;
