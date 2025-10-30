584ad9fbc2331206a5aafd72000e6fc06c989799
elifdenizg <edgokturk@protonmail.com>
2025-10-30 22:04:23 +0100

    [UPDATE] home functional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
+import HomeCard from "../components/HomeCard"
+import Me from "../assets/me.png"
+
+const CardClassName = "text-2xl font-bold p-20 border-1 border-white"
+
 const Home = () => {
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen text-white m-4">
+    <body className="min-h-screen w-full">
+    <div className="flex justify-center items-center flex-col  text-white m-4">
       <h1 className="text-8xl font-black">Elif Deniz Göktürk</h1>
       <div className="columns-3 gap-2 items-center justify-s text-white">
-        <div className="text-2xl font-bold p-20 border-1 border-white">ABOUT ME</div>
-        <div className="text-2xl font-bold p-20 border-1 border-white">PROJECTS</div>
-        <div className="text-2xl font-bold p-20 border-1 border-white">CONTACT</div>
+        <img src={Me} alt="edg" className="w-64 h-auto mt-8 position-fixed" />
+        <HomeCard title="ABOUT ME" url="./AboutMe" />
+        <HomeCard title="PROJECTS" url="./Projects" />
+        <HomeCard title="CONTACT" url="./Contact" />
       </div>
     </div>
+    </body>
   );
 };
 
