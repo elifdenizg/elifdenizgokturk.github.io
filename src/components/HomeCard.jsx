14fe42db497ba18bdd7568ac1d151349c7d89b94
elifdenizg <edgokturk@protonmail.com>
2025-11-03 19:48:48 +0100

    [FIX]

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -6,7 +6,7 @@ function HomeCard({title,url}){
     const navigate = useNavigate();
     return(
         <button type="button" onClick={() => navigate(url)} className="relative group text-5xl font-bold border-2 border-white text-white text-center flex justify-center items-center px-[5vw] py-[25vh]
-        transition-all duration-300 hover:bg-white hover: text-black cursor-pointer">{title}</button>
+        transition-all duration-300 hover:bg-white hover:text-black cursor-pointer">{title}</button>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import HomeCard from "../components/HomeCard";
 import Me from "../assets/me.png";
+import StarBg from "../components/StarBackround";
 
 const Home = () => {
   return (
     <div className="flex justify-start min-h-screen text-white px-16 py-10 max-w-screen ">
-      <div className="flex flex-col gap-10 max-w-3xl ml-[16vw]">
+      <div className="flex flex-col gap-10 max-w mx-[16vw]">
         <div>
           <h1 className="text-7xl font-black mb-6">
             Elif Deniz Göktürk
@@ -18,7 +19,6 @@ const Home = () => {
             pariatur.
           </p>
         </div>
-
         <div className="relative flex gap-4 mt-4 z-0">
           <HomeCard title="ABOUT ME" url="./AboutMe" />
           <HomeCard title="PROJECTS" url="./Projects" />
@@ -29,7 +29,7 @@ const Home = () => {
       <img 
         src={Me}
         alt="Elif Deniz Göktürk"
-        className="absolute bottom-0 left-0 pt-5 h-6/7 w-auto z-10"
+        className="absolute bottom-0 left-0 pt-5 h-[85vh] z-10"
       />
     </div>
   );
