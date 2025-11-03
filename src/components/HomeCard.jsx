311795910818c54f5101c845a57a4f295066c99e
elifdenizg <edgokturk@protonmail.com>
2025-11-03 14:01:33 +0100

    [UPDATE] link to button

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,11 +1,12 @@
 //Les petits carrés de home page
 
+import { useNavigate } from "react-router-dom";
+
 function HomeCard({title,url}){
+    const navigate = useNavigate();
     return(
-        <div className="relative group text-5xl font-bold border-2 border-white text-center flex justify-center items-center px-[5vw] py-[25vh]
-        transition-all duration-300 hover:bg-neutral-200 text-black cursor-pointer">
-            <a href={url} class="relative inset-0 flex items-center justify-center text-white group-hover:text-black">{title}</a>
-        </div>
+        <button type="button" onClick={() => navigate(url)} className="relative group text-5xl font-bold border-2 border-white text-white text-center flex justify-center items-center px-[5vw] py-[25vh]
+        transition-all duration-300 hover:bg-white hover: text-black cursor-pointer">{title}</button>
     );
 }
 
