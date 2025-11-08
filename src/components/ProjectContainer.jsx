10162c56376f7360afc5de50c02177ba52246b3f
elifdenizg <edgokturk@protonmail.com>
2025-11-08 20:24:38 +0100

    [FIX] fix cards

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,16 +1,16 @@
 function ProjectContainer({ urlImg, urlProject, title, skills, description }) {
   return (
     <div
-      className="flex justify-center content-center gap-10 mx-[16vw] border-1 p-3 cursor-pointer"
+      className="flex justify-center content-center gap-10 mx-[16vw] bg-white/75 p-10 cursor-pointer hover:bg-white"
       onClick={() => window.location.replace(urlProject)}
     >
-      <img src={urlImg} className="w-1/2" />
+      <img src={urlImg} className="w-1/2 border-1 " />
       <div className=" flex flex-col w-1/2 justify-center">
-      <div className="my-2">
-        <h3 className="font-semibold text-2xl">{title}</h3>
-        <p className="italic text-sm">{skills}</p>
-      </div>
-        <p className="mr-1">{description}</p>
+        <div className="my-2">
+          <h3 className="font-semibold text-[#131313] text-2xl">{title}</h3>
+          <p className="italic text-sm text-[#131313]">{skills}</p>
+        </div>
+        <p className="mr-1 text-[#131313]">{description}</p>
       </div>
     </div>
   );
